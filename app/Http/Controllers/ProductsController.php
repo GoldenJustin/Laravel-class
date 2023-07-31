9fd7a556bc7884bbb563e50cb7d5bf5eecf115c5
justin <justinemsengi@gmail.com>
2023-07-31 15:32:22 +0300

    alternative ways of passing array data using with

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,12 +9,21 @@ class ProductsController extends Controller
     public function index(){
         $title = "welcome to my laravel 8 course";
         $description = "This is just a beginning";
+        $data = [
+            'productOne'=>'Iphone',
+            'productTwo'=>'Samsung'
+        ];
 
         // width method
-        return view('products.index')->with("title", $title);
+        // return view('products.index')->with("title", $title);
+
+
+        // return view('products.index')->with("data", $data);
 
         // compact way of returning data
         // return view('products.index', compact('title', 'description'));
+
+        return view('products.index', ['data'=> $data]);
     }
 
     public function about(){
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,6 +7,9 @@
 </head>
 <body>
     <h1>products</h1>
-    <p>{{ $title }}</p>
+    @foreach($data as $item)
+
+    <p>{{ $item }}</p>
+    @endforeach
 </body>
 </html>
