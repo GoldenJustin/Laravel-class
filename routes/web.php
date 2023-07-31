50ed1b044af1e5b5694e172660a9a5c34e68a2f2
justin <justinemsengi@gmail.com>
2023-07-31 14:05:14 +0300

    Learning Laravel

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// routes that sends back a view
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// route to users - string
+Route::get('/users', function(){
+    return 'welcome to users page';
+});
+
+// route to users - array(iNTO JSON ALWAYS)
+Route::get('/users', function(){
+    return['IT', 'IS', 'TRIAL'];
+});
+
+// route to users - JSON(iNTO JSON ALWAYS)
+Route::get('/users', function(){
+    return response()->json([
+        'name'=>'JUSTIN',
+        'student'=>'still learning',
+        'content'=>'Laravel class'
+    ]);
+});
+
+
+// firstproject.com ==/
+// firstproject.com/users == /users
